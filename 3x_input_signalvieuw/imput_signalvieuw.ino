8e7dd6a3de4b5b227554f8b18bc2d52ba0ad5800
cabriomike86 <148024674+cabriomike86@users.noreply.github.com>
2023-11-03 06:14:08 +0100

    Update imput_signalvieuw.ino

--- 3x_input_signalvieuw/imput_signalvieuw.ino
+++ 3x_input_signalvieuw/imput_signalvieuw.ino
@@ -21,8 +21,8 @@ void setup(void) {
   tft.fillRect(0, 0,320 , 240, BLACK);
   tft.drawRect(1, 0, 239, 318, WHITE);
   tft.drawRect(9, 9, 181, 71, WHITE);
-  tft.drawLine(10,90, 10, 240, BLUE);
-  tft.drawLine(1, 230, 239, 230, RED);
+  tft.drawLine(10,170, 10, 310, BLUE);
+  tft.drawLine(1, 310, 239, 310, RED);
 
   tft.setTextColor(GREEN);
   tft.setTextSize(1);
@@ -49,6 +49,8 @@ void loop(void) {
   tft.fillRect(map(values[0], 0, 1023, 1, 180) + 9, 10, 180 - map(values[0], 0, 1023, 1, 180), 19, BLACK);
   tft.fillRect(map(values[1], 0, 1023, 1, 180) + 9, 20, 180 - map(values[1], 0, 1023, 1, 180), 29, BLACK);
   tft.fillRect(map(values[2], 0, 1023, 1, 180) + 9, 40, 180 - map(values[2], 0, 1023, 1, 180), 39, BLACK);
+  tft.setTextColor(BLUE);
+
   tft.setCursor(30, 16); tft.println(values[0]);
   tft.setCursor(30, 40); tft.println(values[1]);
   tft.setCursor(30, 65); tft.println(values[2]);
