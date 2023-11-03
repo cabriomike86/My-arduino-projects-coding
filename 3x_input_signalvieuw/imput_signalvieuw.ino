a98dc1c0b05bd24763d0d07a4f5616347b4b3e0f
cabriomike86 <148024674+cabriomike86@users.noreply.github.com>
2023-11-03 06:36:38 +0100

    Update imput_signalvieuw.ino

--- 3x_input_signalvieuw/imput_signalvieuw.ino
+++ 3x_input_signalvieuw/imput_signalvieuw.ino
@@ -16,11 +16,12 @@ void setup(void) {
   tft.begin(identifier);
   tft.setRotation(0);
   tft.fillScreen(BLACK);
-
-
   tft.fillRect(0, 0,320 , 240, BLACK);
+
   tft.drawRect(1, 0, 239, 318, WHITE);
   tft.drawRect(9, 9, 181, 71, WHITE);
+  tft.drawRect(9, 81, 181, 20, WHITE);
+
   tft.drawLine(10,170, 10, 310, BLUE);
   tft.drawLine(1, 310, 239, 310, RED);
 
@@ -32,8 +33,11 @@ void setup(void) {
   tft.setCursor(210, 36); tft.println("Vrx2");
   tft.setCursor(210, 46); tft.println("RSSI ");
 
-  tft.setCursor(210, 61); tft.println("Vrx3");
-  tft.setCursor(210, 71); tft.println("RSSI ");
+  tft.setCursor(210, 60); tft.println("Vrx3");
+  tft.setCursor(210, 70); tft.println("RSSI ");  
+
+  tft.setCursor(191, 82); tft.println("RemoteTX");
+  tft.setCursor(210, 92); tft.println("RSSI ");
 }
 
 void loop(void) {
